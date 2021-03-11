e343fcc427d019a0f3be38fc772ea4ee922b6e4f
Actasclown <heyunqi@pku.edu.cn>
2021-03-11 16:07:03 -0600

    Mar 11 daily challenge. (DP / DFS)

--- src/com/aac/q0322/coinchange/Solution.java
+++ src/com/aac/q0322/coinchange/Solution.java
@@ -3,26 +3,51 @@ package com.aac.q0322.coinchange;
 import java.util.Arrays;
 
 public class Solution {
-    public int coinChange(int[] coins, int amount) {
-        if(coins == null)
-            return 0;
-        int len = coins.length;
-        if(len == 0 || amount <= 0)
-            return 0;
+    
+//    public int coinChange(int[] coins, int amount) {
+//        if(coins == null)
+//            return 0;
+//        int len = coins.length;
+//        if(len == 0 || amount <= 0)
+//            return 0;
+//
+//        int[] dp = new int[amount + 1];
+//        Arrays.fill(dp, amount + 1);
+//        dp[0] = 0;
+//        for(int i = 0;i < len;++i) {
+//            for(int j = coins[i];j <= amount;++j)
+//                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+//        }
+//        int ret = dp[amount];
+//        return ret == amount + 1 ? -1 : ret;
+//    }
 
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
-        for(int i = 0;i < len;++i) {
-            for(int j = coins[i];j <= amount;++j)
-                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+    int minNum = Integer.MAX_VALUE;
+
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        dfs(coins, coins.length - 1, amount, 0);
+        return minNum == Integer.MAX_VALUE ? -1 : minNum;
+    }
+
+    void dfs(int[] coins, int curCoin, int leftAmount, int curUsed) {
+        if(curCoin < 0)
+            return;
+        if(leftAmount % coins[curCoin] == 0) {
+            minNum = Math.min(minNum, curUsed + leftAmount / coins[curCoin]);
+            return;
         }
-        int ret = dp[amount];
-        return ret == amount + 1 ? -1 : ret;
+        for(int i = leftAmount / coins[curCoin];i >= 0;--i) {
+            if(curUsed + i >= minNum)
+                return;
+            else {
+                dfs(coins, curCoin - 1, leftAmount - i * coins[curCoin], curUsed + i);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        int[] coins = {2};
-        System.out.println(new Solution().coinChange(coins, 3));
+        int[] coins = {186,419,83,408};
+        System.out.println(new Solution().coinChange(coins, 6249));
     }
 }
